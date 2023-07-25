243f82b2e51a2967e9a3e0b2c28c7f233a24dad0
Thallison Rhanniel Garcia Lima <thallisonrlima@gmail.com>
2023-07-24 22:00:18 -0300

    Adicionado status bar

--- AppNavigator.tsx
+++ AppNavigator.tsx
@@ -3,12 +3,20 @@ import * as React from 'react';
 import { Button,Text }  from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Cat from './src/common/components/ShowTitleAndInput/Index';
+import { StatusBar } from 'react-native';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const HomeNavigator = () => (
     <Navigator>
-        <Screen name='Home' component={HomeScreen} options={{title: "Tela Inicial"}} />
+        <Screen name='Home' component={HomeScreen} options={{title: "Tela Inicial", headerRight: () => (
+            <Button
+              onPress={() => alert('This is a button!')}
+              title="Info"
+              color="#803434"
+            />
+          ),}} />
         <Screen name='Profile' component={ProfileScreen} options={{title: "Tela de Perfil"}}/>
     </Navigator>
 )
@@ -23,17 +31,26 @@ export const AppNavigator = () => (
 
 const HomeScreen = ({navigation}:any) => {
     return (
-      <Button
+      <>
+        <StatusBar barStyle="dark-content" backgroundColor="#d51616" />
+        <Cat></Cat>
+        <Button
         title="Go to Jane's profile"
         onPress={() =>
           //console.log("teste", navigation)
           navigation.navigate('Profile', {name: 'Jane'})
-        }
-      />
+        }/>
+      </>
     );
 };
 
 const ProfileScreen = ({navigation, route}:any) => {
-    console.log("opaaa", route);
-    return <Text>This is {route.params.name}'s profile</Text>;
+  console.log("opaaa", route);
+  console.log("navigation:route", navigation);
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#d51616" />  
+      <Text>This is {route.params.name}'s profile</Text>
+    </>
+  );
 };
