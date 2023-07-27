c14fc74f23d4e2deb8dc425b5751f75387f409c7
Thallison Rhanniel Garcia Lima <thallisonrlima@gmail.com>
2023-07-26 22:01:49 -0300

    Adicionado os arquivos nativos IOS

---
+++ ios/RCTCalendarModule.h
@@ -0,0 +1,18 @@
+//
+//  RCTCalendarModule.swift
+//  
+//
+//  Created by Thallison Rhanniel Garcia Lima on 26/07/23.
+//
+
+import UIKit
+
+class RCTCalendarModule: NSObject {
+
+}
+
+
+//  RCTCalendarModule.h
+#import <React/RCTBridgeModule.h>
+@interface RCTCalendarModule : NSObject <RCTBridgeModule>
+@end
---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  
+//
+//  Created by Thallison Rhanniel Garcia Lima on 26/07/23.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
